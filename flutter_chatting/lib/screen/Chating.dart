68406ca2d87530ad317683f20219413881cd49cd
Nhatduy209 <nhatduy20000@gmail.com>
2022-03-19 10:50:41 +0700

    Real time chatting

--- flutter_chatting/lib/screen/Chating.dart
+++ flutter_chatting/lib/screen/Chating.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chatting/models/Message.dart';
@@ -40,12 +42,13 @@ class ChattingState extends State<Chatting> {
   late Iterable<Message> listMessagesState = [];
   String id;
   String userChatting;
-  late String username;
-  final List<Message> listMessages;
+  String username = "";
+  late List<Message> listMessages;
+  bool isInitListMessage = true;
   final ScrollController _controller = ScrollController();
   bool isRead = false;
+  late StreamSubscription<QuerySnapshot> listeningMessageChange;
   ChattingState(this.listMessages, this.isRead, this.id, this.userChatting);
-
   final message = TextEditingController();
 
   CollectionReference messages =
@@ -66,8 +69,6 @@ class ChattingState extends State<Chatting> {
 
   Future<dynamic> getListMessage({idChatting = String}) async {
     // Get data from docs and convert map to List
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    username = prefs.getString('username');
 
     var data = await FirebaseFirestore.instance
         .collection('message')
@@ -75,13 +76,16 @@ class ChattingState extends State<Chatting> {
         .collection('listmessage')
         .orderBy('Time', descending: true);
 
-    if (listMessages.isEmpty) {
+    if (listMessages.isEmpty || listMessagesState.isEmpty) {
+      print("LENGTH");
+      print(listMessages.length);
+      print(listMessagesState.length);
       SharedPreferences prefs = await SharedPreferences.getInstance();
       var getUsername = prefs.getString('username');
+      username = prefs.getString('username');
 
       await data.get().then((QuerySnapshot querySnapshot) {
         for (var doc in querySnapshot.docs) {
-          print(doc.data());
           var messageInstance = new Message(id: "", content: "");
           doc.data().forEach((key, value) => {
                 if (key == 'message')
@@ -94,17 +98,47 @@ class ChattingState extends State<Chatting> {
         }
       });
       listMessagesState = listMessages.reversed;
-      if (username.isEmpty) {
-        setState(() => {username: getUsername});
-      }
+      isInitListMessage = false;
+    }
+  }
+
+  @override
+  void initState() {
+    var data = FirebaseFirestore.instance
+        .collection('message')
+        .doc(id)
+        .collection('listmessage');
+    listeningMessageChange =
+        data.snapshots().listen((snapshot) => _onEventsSnapshot(snapshot));
+    super.initState();
+  }
+
+  void _onEventsSnapshot(QuerySnapshot snapshot) {
+    print('EVENT --- change $username');
+    if (isInitListMessage == false) {
+      snapshot.docChanges?.forEach(
+        (docChange) {
+          // If you need to do something for each document change, do it here.
+          var messageInstance = new Message(id: "", content: "");
+          docChange.doc.data().forEach((key, value) => {
+                if (key == 'message')
+                  {messageInstance.setContent = value}
+                else if (key == 'id')
+                  {messageInstance.setId = value},
+              });
+          if (messageInstance.id.contains(username) == false) {
+            listMessages.add(messageInstance);
+            setState(() => {listMessagesState: listMessages});
+          }
+        },
+      );
     }
   }
 
   Widget build(BuildContext context) {
+    print('USERNAME -- $username');
     return FutureBuilder<dynamic>(
-      //   initialData: getUsername(),
-      future:
-          getListMessage(idChatting: id), // function where you call your api
+      future: getListMessage(idChatting: id),
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         // AsyncSnapshot<Your object type>
         if (snapshot.hasError) {
@@ -125,68 +159,51 @@ class ChattingState extends State<Chatting> {
                         Container(
                             margin: const EdgeInsets.only(bottom: 80.0),
                             height: 500.0,
-                            child: StreamBuilder(builder: (BuildContext context,
-                                AsyncSnapshot<int> snapshot) {
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return Text("");
-                              } else {
-                                return ListView(
-                                    children: [
-                                      ...listMessagesState.map((e) => e.id
-                                              .contains(username)
-                                          ? Container(
-                                              width: 200,
-                                              margin: const EdgeInsets.only(
-                                                  top: 5, left: 100),
-                                              decoration: BoxDecoration(
-                                                color: Colors.indigo[50],
-                                                borderRadius:
-                                                    const BorderRadius.only(
-                                                  topRight:
-                                                      Radius.circular(8.0),
-                                                  bottomLeft:
-                                                      Radius.circular(8.0),
-                                                  bottomRight:
-                                                      Radius.circular(8.0),
-                                                  topLeft: Radius.circular(8.0),
-                                                ),
-                                              ),
-                                              child: Padding(
-                                                padding:
-                                                    const EdgeInsets.all(10.0),
-                                                child: Text(
-                                                  e.content,
-                                                  textAlign: TextAlign.right,
-                                                ),
-                                              ))
-                                          : Container(
-                                              margin: const EdgeInsets.only(
-                                                  top: 5, right: 100),
-                                              decoration: const BoxDecoration(
-                                                color: Colors.blue,
-                                                borderRadius: BorderRadius.only(
-                                                  topRight:
-                                                      Radius.circular(8.0),
-                                                  bottomLeft:
-                                                      Radius.circular(8.0),
-                                                  bottomRight:
-                                                      Radius.circular(8.0),
-                                                  topLeft: Radius.circular(8.0),
-                                                ),
-                                              ),
-                                              child: Padding(
-                                                  padding: const EdgeInsets.all(
-                                                      10.0),
-                                                  child: Text(e.content,
-                                                      textAlign:
-                                                          TextAlign.left))))
-                                    ],
-                                    reverse: true,
-                                    shrinkWrap: true,
-                                    controller: _controller);
-                              }
-                            })),
+                            child: ListView(
+                              reverse: true,
+                              shrinkWrap: true,
+                              controller: _controller,
+                              children: [
+                                ...listMessagesState.map((e) => e.id
+                                        .contains(username)
+                                    ? Container(
+                                        width: 200,
+                                        margin: const EdgeInsets.only(
+                                            top: 5, left: 100),
+                                        decoration: BoxDecoration(
+                                          color: Colors.indigo[50],
+                                          borderRadius: const BorderRadius.only(
+                                            topRight: Radius.circular(8.0),
+                                            bottomLeft: Radius.circular(8.0),
+                                            bottomRight: Radius.circular(8.0),
+                                            topLeft: Radius.circular(8.0),
+                                          ),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(10.0),
+                                          child: Text(
+                                            e.content,
+                                            textAlign: TextAlign.right,
+                                          ),
+                                        ))
+                                    : Container(
+                                        margin: const EdgeInsets.only(
+                                            top: 5, right: 100),
+                                        decoration: const BoxDecoration(
+                                          color: Colors.blue,
+                                          borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(8.0),
+                                            bottomLeft: Radius.circular(8.0),
+                                            bottomRight: Radius.circular(8.0),
+                                            topLeft: Radius.circular(8.0),
+                                          ),
+                                        ),
+                                        child: Padding(
+                                            padding: const EdgeInsets.all(10.0),
+                                            child: Text(e.content,
+                                                textAlign: TextAlign.left))))
+                              ],
+                            )),
                         Positioned(
                             child: Row(children: [
                               Container(
